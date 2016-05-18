435dab714060149ec32cba6d361433f00cf5e5ba
andrewgurung <andrewgurungg@gmail.com>
2016-05-17 21:33:13 -0400

    HTML Element using JSX expression

--- excercises/1-your-first-component/app.js
+++ excercises/1-your-first-component/app.js
@@ -3,17 +3,31 @@ var React = require('react');
 // Object destructuring assignment
 var { h1, div, li, ul } = React.DOM;
 
-// HTML Element
-// Nested HTML element using React DOM
-// Each element is actually written as:
-// React.DOM.h1(attributes, content)
-var element = div({},
-                  h1({}, "Hello"),
-                  ul({},
-                    li({}, "React"),
-                    li({}, "Angular")
-                  )
-              );
+// HTML Element using React DOM functions
+// Each element is actually written as: React.DOM.h1(attributes, content)
+// These are all function calls with arguments returned as children of that element
+var element = (
+  div({},
+    h1({}, "Hello"),
+    ul({},
+      li({}, "React"),
+      li({}, "Angular")
+    )
+  )
+);
+
+// HTML Element using JSX expression
+// Just another way to call functions
+// More commonly used than React.DOM functions
+var element2 = (
+  <div>
+    <h1>Hello</h1>
+    <ul>
+      <li>React</li>
+      <li>Angular</li>
+    </ul>
+  </div>
+);
 
 // React.render(what, where)
-React.render(element, document.body);
+React.render(element1, document.body);
