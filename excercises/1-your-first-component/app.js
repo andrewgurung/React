5664be8d9529d0d4ec95c8af615baad0d3d6e5bd
andrewgurung <andrewgurungg@gmail.com>
2016-05-17 21:11:32 -0400

    Object destructuring assignment for h1

--- excercises/1-your-first-component/app.js
+++ excercises/1-your-first-component/app.js
@@ -1,7 +1,10 @@
 var React = require('react');
 
+// Object destructuring assignment
+var { h1 } = React.DOM;
+
 // HTML Element
-var element = React.DOM.h1({}, "Hello");
+var element = h1({}, "Hello");
 
 // React.render(what, where)
 // React.DOM.h1(attributes, content)
