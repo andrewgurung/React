990a5ab4b2120e82c5760893708b9462c8292c2b
andrewgurung <andrewgurungg@gmail.com>
2016-05-17 21:07:09 -0400

    Refactor HTML Element

--- excercises/1-your-first-component/app.js
+++ excercises/1-your-first-component/app.js
@@ -1,5 +1,8 @@
 var React = require('react');
 
+// HTML Element
+var element = React.DOM.h1({}, "Hello");
+
 // React.render(what, where)
 // React.DOM.h1(attributes, content)
-React.render(React.DOM.h1({}, "Hello"), document.body);
+React.render(element, document.body);
