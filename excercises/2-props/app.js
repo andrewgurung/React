fc0d462e1a6f9bc163b18e33abb018933c04aac2
andrewgurung <andrewgurungg@gmail.com>
2016-06-15 21:38:45 -0400

    Complete first lesson Gravatar

--- excercises/2-props/app.js
+++ excercises/2-props/app.js
@@ -2,22 +2,25 @@ var React = require('react');
 var md5 = require('MD5');
 
 var GRAVATAR_URL = "http://gravatar.com/avatar";
+
 var USERS = [
   { id: 1, name: 'Ryan Florence', email: 'rpflorencegmail.com' },
   { id: 2, name: 'Michael Jackson', email: 'mjijackson@gmail.com' }
 ];
 
+var Gravatar = React.createClass({
+  render () {
+    var size = 36;
+    var hash = md5(this.props.email);
+    var url = `${GRAVATAR_URL}/${hash}?s=${size*2}`;
+    return <img src={url} width={size} />;
+  }
+})
+
 var App = React.createClass({
   render () {
     var users = USERS.map((user) => {
-      var size = 36;
-      var hash = md5(user.email);
-      var url = `${GRAVATAR_URL}/${hash}?s=${size*2}`;
-      return (
-        <li>
-          <img src={url} width={size} /> {user.name}
-        </li>
-      );
+      return <li><Gravatar email={user.email}/> {user.name}</li>
     });
     return (
       <div>
